7cfbd9172ec1836575d6faba8eef1b8710bc9845
Brandon Paree <brandon.paree@gmail.com>
2018-04-19 01:31:12 -0700

    Add background image to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,44 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Img from 'gatsby-image'
 
-const IndexPage = () => (
-  <section className="hero custom-hero is-dark is-fullheight">
-    <div className="hero-body">
-      <div className="container">
-        <div className="columns">
-          <div className="column is-mobile is-one-third box has-text-right">
-            <h1 className="title">Brandon Paree</h1>
-            <h2 className="subtitle">Full Stack Developer</h2>
+const IndexPage = ({ data }) => {
+  return (
+    <section className="hero custom-hero is-dark is-fullheight">
+      <div className="hero-body">
+        <Img sizes={data.file.childImageSharp.sizes} />
+        <div className="container">
+          <div className="columns">
+            <div className="column is-mobile is-one-third box has-text-right">
+              <h1 className="title">Brandon Paree</h1>
+              <h2 className="subtitle">Full Stack Developer</h2>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
-)
+    </section>
+  )
+}
 
 export default IndexPage
+
+export const imageQuery = graphql`
+query ImageQuery {
+  file(relativePath: { regex: "/background.jpg/" }) {
+    childImageSharp {
+      sizes(quality: 100, maxWidth: 1200) {
+        base64
+        tracedSVG
+        aspectRatio
+        src
+        srcSet
+        srcWebp
+        srcSetWebp
+        sizes
+        originalImg
+        originalName
+      }
+    }
+  }
+}
+`;
